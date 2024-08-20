176ad4b86a138069d339088fff40c11c1cd9f2ba
pyc-chan <rtrtoioil@gmail.com>
2024-08-20 13:12:54 +0900

    Update Ex99_Main.java

--- Ex99_Main.java
+++ Ex99_Main.java
@@ -9,27 +9,32 @@ public class Ex99_Main {
 		Scanner scan =new Scanner(System.in);
 		HashSet<Ex99> k = new HashSet<Ex99>();
 		esc : while(true) {
-			System.out.println("이름 : ")	;
-			String name = scan.next();
-			System.out.println("국어 : ");
-			int kor = scan.nextInt();
-			System.out.println("영어 : ");
-			int eng = scan.nextInt();
-			System.out.println("수학 : ");
-			int math = scan.nextInt();
-			k.add(new Ex99(name, kor, eng, math));
-			while(true) {
-				System.out.println("계속 하시겠습니까?(Y/N)");
-				String tmp = scan.next();
-				if (tmp.equalsIgnoreCase("y")){
-					continue esc;
-				}else if(tmp.equalsIgnoreCase("n")) {
-					break esc;
-				}else {
-					System.out.println("Y 또는 N을 입력하세요");
+			try{
+				System.out.println("이름 : ")	;
+				String name = scan.next();
+				System.out.println("국어 : ");
+				int kor = scan.nextInt();
+				System.out.println("영어 : ");
+				int eng = scan.nextInt();
+				System.out.println("수학 : ");
+				int math = scan.nextInt();
+				k.add(new Ex99(name, kor, eng, math));
+				while(true) {
+					System.out.println("계속 하시겠습니까?(Y/N)");
+					String tmp = scan.next();
+					if (tmp.equalsIgnoreCase("y")){
+						continue esc;
+					}else if(tmp.equalsIgnoreCase("n")) {
+						break esc;
+					}else {
+						System.out.println("Y 또는 N을 입력하세요");
+					}
 				}
+			}catch(InputMismatchException e){
+				System.out.println("올바른 값을 입력하세요");
+				scan.nextLine();
+				continue esc;
 			}
-		
 		}
 		System.out.println("이름\t국어\t영어\t수학\t총점\t평균\t학점");
 		Iterator<Ex99> it = k.iterator();
